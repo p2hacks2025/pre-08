6494a0e4ab35e9fe00af335d976620c393699cec
Yuzuki Kaya <b1025054@fun.ac.jp>
2025-12-20 21:16:30 +0900

    ギミックオブジェクトを動かしやすいように改善 サンプルステージを作成 ステージ選択システムを仮実装(SelectScene.cs)

--- Assets/Scripts/Base/SelectScene.cs
+++ Assets/Scripts/Base/SelectScene.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectScene : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Text stageNumberText;      //ステージナンバー表示用テキスト    
+    [SerializeField] private int minStage = 1;          //ステージの最小値
+    [SerializeField] private int maxStage = 10;         //ステージの最大値
+    private ChangeScene changeScene;                     //シーン遷移用スクリプト参照
+    private int currentStage = 1;                       //現在選択されているステージ
+    
     void Start()
-    {
-        
+    {   
+        //コンポーネントの取得
+        changeScene = GetComponent<ChangeScene>();
+        //初期ステージナンバーを表示
+        UpdateStageDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PrevStage()
     {
+        if (currentStage > minStage)
+        {
+            currentStage--;
+            UpdateStageDisplay();
+        }
+    }
+    public void NextStage()
+    {
+        if (currentStage < maxStage)
+        {
+            currentStage++;
+            UpdateStageDisplay();
+        }
+    }
+    public void DecideStage()
+    {
+        //選択されたステージシーンを読み込む
+        changeScene.LoadScene("Stage" + currentStage);
+    }
+    private void UpdateStageDisplay()
+    {
+        if (stageNumberText != null)
+        {
+            stageNumberText.text = "Stage " + currentStage.ToString();
+        }
         
+        //ボタンの有効/無効を更新
+        UpdateButtonStates();
+    }
+    private void UpdateButtonStates()
+    {
+        //Prevボタンの有効/無効設定
+        Button prevButton = GameObject.Find("PrevBtn").GetComponent<Button>();
+        if (prevButton != null)
+        {
+            prevButton.interactable = currentStage > minStage;
+        }
+
+        //Nextボタンの有効/無効設定
+        Button nextButton = GameObject.Find("NextBtn").GetComponent<Button>();
+        if (nextButton != null)
+        {
+            nextButton.interactable = currentStage < maxStage;
+        }
     }
 }
